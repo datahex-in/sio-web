901c2e25799683ee1ad7c61768316bbda338bf52
Adhil Akbar <356adhil@gmail.com>
2023-09-16 13:03:44 +0530

    Completed Update section.

---
+++ routes/ejsRoutes/updatesEjs.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const News = require("../../models/news");
+const Testimonial = require("../../models/testimonial");
+const Gallery = require('../../models/gallery')
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  try {
+    const newsData = await News.find();
+    const articleData = await News.find();
+    const testimonialData = await Testimonial.find();
+    const galleryData = await Gallery.find();
+    console.log(newsData);
+    console.log(testimonialData);
+    console.log(galleryData);
+    res.render("updates", { newsData, testimonialData, galleryData, articleData });
+  } catch (error) {
+    console.log(error)
+    console.error(error);
+  }
+});
+
+module.exports = router;
