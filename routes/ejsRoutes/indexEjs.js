572a82a2a160e1533dfa8a24c4becd34958e9c6e
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-09-25 15:17:21 +0530

    testimonial completed in home

--- routes/ejsRoutes/indexEjs.js
+++ routes/ejsRoutes/indexEjs.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AboutUs = require("../../models/aboutUs");
 const Event = require("../../models/event");
+const Testimonial = require("../../models/testimonial");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
@@ -22,9 +23,16 @@ router.get("/", async function (req, res, next) {
     console.log(eventTypeCounts);
     const aboutData = await AboutUs.findOne();
     const eventData = await Event.find();
+    const testimonialData = await Testimonial.find();
+    console.log("testimonial Data :- ", testimonialData);
     console.log(aboutData);
     console.log(eventData);
-    res.render("index", { aboutData, eventData, eventTypeCounts });
+    res.render("index", {
+      aboutData,
+      eventData,
+      eventTypeCounts,
+      testimonialData,
+    });
   } catch (error) {
     console.error("Error:", error);
   }
