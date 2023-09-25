ba5e332bbf7c74d738fa59a905c20ae2db703631
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-09-25 13:06:26 +0530

    event page - header image- hard coded fixed.

--- routes/event.js
+++ routes/event.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 // Controllers
 const {
-    createEvent,
-    getEvent,
-    updateEvent,
-    deleteEvent,
-    select,
+  createEvent,
+  getEvent,
+  updateEvent,
+  deleteEvent,
+  select,
 } = require("../controllers/event");
 // Middleware
 const { protect, authorize } = require("../middleware/auth");
@@ -14,19 +14,19 @@ const { getS3Middleware } = require("../middleware/s3client");
 const getUploadMiddleware = require("../middleware/upload");
 
 router
-    .route("/")
-    .post(
-        getUploadMiddleware("uploads/event", ["image"]),
-        getS3Middleware(["image"]),
-        createEvent
-    )
-    .get(reqFilter, getEvent)
-    .put(
-        getUploadMiddleware("uploads/event", ["image"]),
-        getS3Middleware(["image"]),
-        updateEvent
-    )
-    .delete(deleteEvent);
+  .route("/")
+  .post(
+    getUploadMiddleware("uploads/event", ["image", "headerImage"]),
+    getS3Middleware(["image", "headerImage"]),
+    createEvent
+  )
+  .get(reqFilter, getEvent)
+  .put(
+    getUploadMiddleware("uploads/event", ["image", "headerImage"]),
+    getS3Middleware(["image", "headerImage"]),
+    updateEvent
+  )
+  .delete(deleteEvent);
 
 router.get("/select", reqFilter, select);
 
